395e08ab5890a201e7990bb858d149841e248d42
Amitesh <amiteshr90@gmail.com>
2019-08-07 05:01:24 +0530

    part 2 calls done

---
+++ src/components/CallList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import agent from '../agent';
+import { connect } from 'react-redux';
+
+function agentSelected(item, props) {
+  console.log(item);
+  // props.parentCallback(item);
+}
+
+
+function showList(props) {
+  var listItems = props.items.map(function(item, index) {
+    return (
+      <div key={index} onClick={() => agentSelected(item, props)}>{item}</div>
+    );
+  });
+  return listItems;
+}
+
+const CallList = props => {
+  var listItems = null;
+
+  listItems = showList(props);
+
+  return (
+    <div>
+      <div className="inline-list">
+        <div>{listItems}</div>
+      </div>
+    </div>
+  );
+  
+};
+
+export default connect()(CallList);
